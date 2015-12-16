80b2642b2cdf59a18c39cfe93421f85c8ca38a09
zvirgilio <zvirgilio@wustl.edu>
2015-12-15 21:28:55 -0600

    First Github commit with tip application

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,78 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Zachary Virgilio on 12/12/15.
+//  Copyright © 2015 Zachary Virgilio. All rights reserved.
+//
+
+import UIKit
+
+protocol SettingsViewControllerDelegate {
+    func updateTipRate(data: Double)
+    
+}
+
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var tipRatePicker: UIPickerView!
+
+    var delegate: SettingsViewControllerDelegate?
+    
+    var tipRatePickerData: [String] = [String]()
+    var newTipRate = 0.2
+    
+    // Array of double tip rates
+    let tipRates = [0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.22, 0.24, 0.26, 0.28, 0.30]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // Connect Data
+        self.tipRatePicker.delegate = self
+        self.tipRatePicker.dataSource = self
+        
+        // Input data to the Array
+        tipRatePickerData = ["10%", "12%", "14%", "16%", "18%", "20%", "22%", "24%", "26%", "28%", "30%"]
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    
+    //The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tipRatePickerData.count
+    }
+
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tipRatePickerData[row]
+    }
+    
+    //Capture picker view data selection
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //TO DO: check if this works
+            newTipRate = tipRates[tipRatePicker.selectedRowInComponent(0)]
+        
+            self.delegate?.updateTipRate(newTipRate)
+    }
+    // Store new user data on exit
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(newTipRate, forKey: "updatethiskeyplease")
+        defaults.synchronize()
+    }
+
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var tipRateLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    // Settings View Controller Delegate methods
+    func controller(controller: SettingsViewController, passedTipRate: Double) {
+        // Update passedTipRate
+        tipRate = passedTipRate
+    }
+    
+    var tipRate: Double!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        // For users with no stored data
+        tipRateLabel.text = "20.0%"
+        tipRate = 0.20
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Update the tip and total caclulations
+    @IBAction func updateTip() {
+        let tipPercentage = tipRate
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        
+        tipLabel.text = "$\(tip)"
+        tipRateLabel.text = "$\(100*tipRate)%"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        updateTip()
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "settingsSegue"){
+            (segue.destinationViewController as! SettingsViewController).delegate = self
+            }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        self.tipRate = defaults.doubleForKey("updatethiskeyplease")
+        updateTip()
+    }
+   
 }
 
+
+extension ViewController: SettingsViewControllerDelegate {
+    func updateTipRate(data: Double) {
+        self.tipRate = data
+    }
+}
